99984d71942fb68419bd7037fab981fdd8973497
Mat Marcus <mmarcus@emarcus.org>
2011-03-02 20:35:27 -0800

    another move to mmarcus repo

--- recipes/el-get.el
+++ recipes/el-get.el
@@ -1,6 +1,6 @@
 (:name el-get
        :type git
-       :url "https://github.com/dimitri/el-get.git"
+       :url "https://github.com/mmarcus/el-get.git"
        :features el-get
        :load    "el-get.el"
        :compile "el-get.el")
